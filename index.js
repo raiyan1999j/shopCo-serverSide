b895bd2129880a9affc0ca3be9c83696b0065057
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-09-29 23:26:44 +0600

    add new product api

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqywi72.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const app = express();
 const port= 3000;
+
+app.use(cors());
+app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,10 +21,41 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const database = client.db("e-commerce");
+    const userInfo = database.collection("userInfo");
+    const allProducts=database.collection("allProducts");
+
+    // create new account
+    app.post('/createNewAccount', async(req,res)=>{
+        const container = req.body;
+        const result = await userInfo.insertOne(container);
+    })
+    // get user info
+    app.get('/userInfo',async (req,res)=>{
+      const container = req.query.email;
+      const query = {email:container};
+      const option={
+        projection:{_id:0,type:1}
+      }
+
+      const result = await userInfo.findOne(query,option);
+
+      res.send(result);
+    })
+    // add new products
+    app.post('/addNewProduct', async(req,res)=>{
+      const container = req.body;
+      const {title,subTitle} = req.query;
+      const exist = {[title]:{$exists:true}}
+      const query = await allProducts.findOne(exist);
+      const wrap = {[title]:[{[subTitle]:container}]};
+
+      if(!query){
+        await allProducts.insertOne(wrap);
+      }else{
+        await allProducts.updateOne({_id:new ObjectId(`${query._id}`)},{$push:{[title]:{[subTitle]:container}}});
+      }
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
